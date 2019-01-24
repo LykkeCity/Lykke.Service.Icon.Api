751e1e0494f919066174964fb2664e99ba7aae4b
AleksandrBelousov <tortyt1@gmail.com>
2019-01-24 15:31:36 +0400

    added nonce strategy

---
+++ src/Lykke.Service.Icon.Api.Services/NonceStrategy.cs
@@ -0,0 +1,15 @@
+﻿using Lykke.Quintessence.Domain.Services;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Lykke.Service.Icon.Api.Services
+{
+    public class NonceService : INonceService
+    {
+        //just a fake
+        public Task<BigInteger> GetNextNonceAsync(string address)
+        {
+            return Task.FromResult(BigInteger.One);
+        }
+    }
+}
