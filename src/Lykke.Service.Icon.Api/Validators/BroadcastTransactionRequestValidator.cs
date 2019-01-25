611b0d4901aeda631ead626cab30b49d2c1dad20
AleksandrBelousov <tortyt1@gmail.com>
2019-01-25 20:06:37 +0400

    Added validation for broadcast method

--- src/Lykke.Service.Icon.Api/Validators/BroadcastTransactionRequestValidator.cs
+++ src/Lykke.Service.Icon.Api/Validators/BroadcastTransactionRequestValidator.cs
@@ -1,6 +1,10 @@
-﻿using FluentValidation;
+﻿using System;
+using FluentValidation;
+using FluentValidation.Validators;
 using JetBrains.Annotations;
+using Lykke.Icon.Sdk;
 using Lykke.Service.BlockchainApi.Contract.Transactions;
+using Lykke.Service.Icon.Api.Core.Helpers;
 
 namespace Lykke.Service.Icon.Api.Validators
 {
@@ -11,7 +15,27 @@ namespace Lykke.Service.Icon.Api.Validators
         {
             Quintessence.Validators.Rules.TransactionIdMustBeNonEmptyGuid(RuleFor(x => x.OperationId));
             
-            RuleFor(x => x.SignedTransaction).NotNull();
+            RuleFor(x => x.SignedTransaction)
+                .NotNull()
+                .Custom(ValidateSignedTransaction);
+        }
+
+        private void ValidateSignedTransaction(string signedTransaction, CustomContext context)
+        {
+            try
+            {
+                string txDataStr = System.Text.Encoding.UTF8.DecodeBase64(signedTransaction);
+                var transaction = SignedTransaction.Deserialize(txDataStr);
+                var props = transaction.GetProperties();
+                var signature = props.GetItem("signature");
+
+                if (string.IsNullOrEmpty(signature?.ToString()))
+                    context.AddFailure("SignedTransaction", "Signature is empty");
+            }
+            catch (Exception e)
+            {
+                context.AddFailure("SignedTransaction", e.Message);
+            }
         }
     }
 }
